b25ce8b21571d5db2381ca21feaf2fd31bdf9c59
Craig <craigjope@gmail.com>
2021-06-10 19:11:50 -0600

    commit tree 92454af5d0cce945df2641ba37e05ee81db4dc58

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(result) {
+  if (result <= 400) {
+    return result = "This one is on me!"
+  } else if (result > 2000 && result < 2500) {
+    return result = "I will gladly take your thirty bucks."
+  } else if (result > 2500) {
+    return result = "No can do."
+  }
+}
+//changer result to scubaresponse/depth?
+
+function ternaryCheckCity(destination) {
+  if (destination == "NYC") {
+    return destination = "Ok, sounds good."
+  } else {
+    return destination = "No go."
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
-
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip) {
+  if (tip == "generous") {
+    return tip = "Thank you so much."
+  } else if (tip == "not as generous") {
+    return tip = "Thank you."
+  } else {
+    return "Bye."
+  }
 }
